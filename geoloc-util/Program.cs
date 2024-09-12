17f4cde3150ea08e83c1697dd68112901193c6b5
Gottsberger <brian.gottsberger@gmail.com>
2024-09-12 08:06:20 -0500

    Add more detailed instructions for console

--- geoloc-util/Program.cs
+++ geoloc-util/Program.cs
@@ -3,11 +3,12 @@ using geoloc_util;
 using geoloc_util.Utilities;
 
 string[] userInput;
-string[] input;
 
 if (args.Length == 0)
 {
-    Console.WriteLine("Please enter the ZIP code(s) you would like to return location information on:");
+    Console.WriteLine("Please enter the city name or ZIP code(s) you would like to return location information on:");
+    Console.WriteLine("Format should be: \"City, ST\" or \"12345\"");
+    Console.WriteLine("Multiple inputs are supported, ex: \"City, ST\" \"12345\"");
     string manualInput = Console.ReadLine().Trim('"');
     userInput = StringUtilities.ReplaceSubstring(manualInput, "\" \"", ";").Split(';');
 }
